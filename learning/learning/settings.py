cbfc8b5dc7d705d0d5445a10a447f9049202a723
Ageligeli Moses <ageligeliakataankamoses@gmail.com>
2026-04-08 00:11:55 +0300

    Fix deployment setup: add gunicorn, whitenoise, Procfile

--- learning/learning/settings.py
+++ learning/learning/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'] = dj_database_url(
+DATABASES['default'] = dj_database_url.config(
     default=f"sqlite:///{BASE_DIR / 'db.sqlite'}"
 )
 
